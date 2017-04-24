53950b19922fd8e3e232a4043c17ec046ee9a996
Adi Shavit @ MacBookPro <adi.shavit@autodesk.com>
2017-04-24 09:24:09 +0300

    Example with span.

--- sum_post.js
+++ sum_post.js
@@ -2,3 +2,7 @@
 Module["sum_c"] = function(uintArray){
   return Module.ccall('sum', 'number',['array','number'], [uint8Array.buffer, uint8Array.length]);
 };
+
+Module["sum"] = function(span){
+  return Module._sum(span.ptr, span.length);;
+};
